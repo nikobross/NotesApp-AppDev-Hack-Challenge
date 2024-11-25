f36d0475cf292ae9d844d8bc505eb1ccaad35a1a
Jason Wang <jcw348@cornell.edu>
2024-11-25 17:01:16 -0500

    Pushing user db

--- db.py
+++ db.py
@@ -7,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+    pass
     
 
 class Note(db.Model):
