969aa581a95225e613884e98e4aef28aea3b92f6
Niko Ross <niko.bear.ross@gmail.com>
2024-11-25 17:01:45 -0500

    merge conflict

--- db.py
+++ db.py
@@ -3,12 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
     pass
-    
 
 class Note(db.Model):
     __tablename__ = "notes"
